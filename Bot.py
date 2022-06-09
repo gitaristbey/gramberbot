3c9e37c7f91dcd370e877ee9473a5c3770fc772e
gitaristbey <84524246+gitaristbey@users.noreply.github.com>
2022-06-09 20:10:21 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -69,68 +69,6 @@ async def help(event):
     link_preview=False,
     )
 
-@client.on(events.NewMessage(pattern="^/gradmin ?(.*)"))
-async def mentionall(event):
-  chat_id = event.chat_id
-  if event.is_private:
-    return await event.respond("__Bu komut gruplarda ve kanallarda kullanılabilir.!__")
-  
-  is_admin = False
-  try:
-    partici_ = await client(ChannelParticipantsAdmins(
-      event.chat_id,
-      event.sender_id
-    ))
-  except UserNotParticipantError:
-    is_admin = False
-  else:
-    if (
-      isinstance(
-        partici_.participant,
-        (
-          ChannelParticipantsAdmins
-        )
-      )
-    ):
-      is_admin = True
-  if not is_admin:
-    return await event.respond("__Yalnızca yöneticiler Kullanabilir!__")
-  
-  if event.pattern_match.group(1) and event.is_reply:
-    return await event.respond("__Bana bir argüman ver!__")
-  elif event.pattern_match.group(1):
-    mode = "text_on_cmd"
-    msg = event.pattern_match.group(1)
-  elif event.is_reply:
-    mode = "text_on_reply"
-    msg = await event.get_reply_message()
-    if msg == None:
-        return await event.respond("__Eski mesajlar için üyelerden bahsedemem! (gruba eklenmeden önce gönderilen mesajlar)__")
-  else:
-    return await event.respond("__Bir mesajı yanıtlayın veya başkalarından bahsetmem için bana bir metin verin!__")
-  
-  spam_chats.append(chat_id)
-  usrnum = 0
-  usrtxt = ''
-  async for usr in client.iter_participants(chat_id):
-    if not chat_id in spam_chats:
-      break
-    usrnum += 1
-    usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-    if usrnum == 5:
-      if mode == "text_on_cmd":
-        txt = f"{usrtxt}\n\n{msg}"
-        await client.send_message(chat_id, txt)
-      elif mode == "text_on_reply":
-        await msg.reply(usrtxt)
-      await asyncio.sleep(2)
-      usrnum = 0
-      usrtxt = ''
-  try:
-    spam_chats.remove(chat_id)
-  except:
-    pass
-
 
 @client.on(events.NewMessage(pattern="^/grall ?(.*)"))
 async def mentionall(event):
